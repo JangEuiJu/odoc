d6946aaad9e195c9a8d0fbfbc5675fd099955432
JangEuiJu <142605401+JangEuiJu@users.noreply.github.com>
2025-01-28 11:38:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		long a, b, c, sum;
+		a = input.nextLong();
+		b = input.nextLong();
+		c = input.nextLong();
+		sum = a+b+c;
+				
+		System.out.println(sum);
+	
+	}
+
+}
