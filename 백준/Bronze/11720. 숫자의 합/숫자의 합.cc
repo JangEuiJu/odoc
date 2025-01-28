c1dc36be3865fde420ac8abb87f63ca0076d2f28
JangEuiJu <142605401+JangEuiJu@users.noreply.github.com>
2025-01-28 11:40:15 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{	
+	char s[100];
+	int n, sum = 0;
+
+	scanf("%d", &n);
+	scanf("%s", s);
+
+	for (int i = 0; i < n; i++) {
+		sum += s[i] - '0';
+	}
+	printf("%d", sum);
+
+	return 0;
+}
