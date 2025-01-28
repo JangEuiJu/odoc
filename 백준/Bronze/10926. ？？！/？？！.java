0bf2e8897ad9551374bba862f6c621f5297bc74f
JangEuiJu <142605401+JangEuiJu@users.noreply.github.com>
2025-01-28 11:36:25 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String x = input.next();
+
+		System.out.println(x+"??!");
+
+	}
+
+}
