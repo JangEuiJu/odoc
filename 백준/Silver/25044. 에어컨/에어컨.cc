c969dd2cb342c8bc02bb333b41dc6375b977d218
JangEuiJu <142605401+JangEuiJu@users.noreply.github.com>
2025-09-22 06:14:30 +0900

    [Silver IV] Title: 에어컨, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/25044. 에어컨/에어컨.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+string Ch(int x) {
+	string s = "";
+	string hour = to_string(x / 60);
+	string minute = to_string(x % 60);
+	if (LEN(hour) == 1) {
+		s += '0';
+	}
+	s += hour;
+	s += ':';
+	if (LEN(minute) == 1) {
+		s += '0';
+	}
+	s += minute;
+	return s;
+}
+
+pii Calc(pii x, int add) {
+	return { (x.first + add) % 1440, (x.first + add >= 1440 ? x.second + 1 : x.second)};
+}
+
+int main() {
+	FASTIO;
+	int N, K;
+	cin >> N >> K;
+	pii timing = { 900, 0 };
+	int add[3] = { 180, 180, 1080 + K };
+	int cnt = 0;
+	vector<int> v;
+	int idx = 0;
+	while (timing.second <= N) {
+		if (timing.second == N) {
+			v.push_back(timing.first);
+		}
+		timing = Calc(timing, add[idx]);
+		idx = (idx + 1) % 3;
+	}
+
+	cout << LEN(v) << "\n";
+	for(int x : v) {
+		cout << Ch(x) << "\n";
+	}
+
+	return 0;
+}
